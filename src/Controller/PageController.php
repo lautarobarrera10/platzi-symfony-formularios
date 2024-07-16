6fa0fe0bc83d8c1e6a1013842d9f06786e8ea877
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-07-15 22:59:13 -0300

    Nuestro primer formulario en Symfony 6

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,16 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
-    public function index(): Response
+    #[Route('/contactos-v1', methods:['GET', 'POST'])]
+    public function contactV1(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
+        $form = $this->createFormBuilder()
+            ->add('email', TextType::class)
+            ->add('message', TextareaType::class, [
+                'label' => 'Comentario'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enviar'
+            ])
+            ->setMethod('GET')
+            ->setAction('otra-url')
+            ->getForm();
+
+        return $this->render('page/contact-v1.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
