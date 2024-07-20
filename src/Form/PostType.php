c6a44fe9a7057ae0a47dc0eb65de9fa216730ad1
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-07-20 17:55:58 -0300

    Control de elección

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,13 +7,40 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'Lenguajes' => [
+                        'PHP' => 'php'
+                    ],
+                    'Frameworks' => [
+                        'Laravel' => 'laravel',
+                        'Symfony' => 'symfony'
+                    ]
+                ],
+                'placeholder' => 'Seleccione una ...',
+                'label' => 'Categorías'
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Título de la publicación',
+                'help' => 'Piensa en el SEO ¿Cómo buscarías en Google?'
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Contenido',
+                'attr' => ['rows' => 9]
+            ])
+            ->add('Enviar', SubmitType::class, [
+                'attr' => ['class' => 'btn-dark']
+            ])
         ;
     }
 
@@ -21,6 +48,8 @@ class PostType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
+            // 'csrf_protection' => false
+            // 'csrf_field_name' => 'token_personalizado'
         ]);
     }
 }
